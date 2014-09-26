52ab54c54b9dc4b30bf012e0539bde74d3431f5c
Andrii <shatskyat@gmail.com>
2014-09-26 09:36:18 +0300

    started page Contacts

---
+++ contacts.php
@@ -0,0 +1,8 @@
+<?php
+	include_once('inc/head.php');
+	include_once('inc/header.php');
+
+	include_once('inc/address.php');
+
+	include_once('inc/footer.php');
+?>
---
+++ inc/address.php
@@ -0,0 +1,17 @@
+
+
+<section class="address">
+	<div class="container">
+		<div class="row">
+			<div class="span12">
+				<div class="top-section">
+					<div class="up-section">
+						<i class="icon-map"></i>
+					</div>
+					<h1>Контакты</h1>
+					<h2>Есть вопросы? Мы поможем! Свяжитесь с нами удобным для нас способом.</h2>					
+				</div>				
+			</div>
+		</div>		
+	</div>	
+</section>
