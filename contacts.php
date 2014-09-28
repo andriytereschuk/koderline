d9555cc4ff7a65791dc0498c4f5c4a35f046eb8b
Andrii <shatskyat@gmail.com>
2014-09-28 16:59:08 +0300

    Contacts

--- contacts.php
+++ contacts.php
@@ -3,6 +3,8 @@
 	include_once('inc/header.php');
 
 	include_once('inc/address.php');
+	include_once('inc/managers.php');
+	include_once('inc/map.php');			
 
 	include_once('inc/footer.php');
 ?>
---
+++ js/map.js
@@ -0,0 +1,32 @@
+function init() {
+	var latLng = new google.maps.LatLng(55.781928,37.7);
+
+	var settings = {
+		center: latLng,
+		zoom: 15,
+		scrollwheel: false,
+		mapTypeControl: true,
+		mapTypeControlOptions: {style: google.maps.MapTypeControlStyle.DROPDOWN_MENU},
+		navigationControl: true,
+		navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL},
+		mapTypeId: google.maps.MapTypeId.ROADMAP,
+		styles: [{
+			"stylers": [
+				{ "saturation": -100 },
+				{ "lightness": -8 }
+			]
+		}]
+	};
+
+	var map = new google.maps.Map(document.getElementById("map"), settings);
+	
+	var marker = new google.maps.Marker({
+		map: map,
+		position: new google.maps.LatLng(55.781928,37.7134395),
+		draggable: false,
+		title: 'Главный офис',
+		icon:'images/icons/icon-marker.png'
+	});
+}
+
+google.maps.event.addDomListener(window, 'load', init);
