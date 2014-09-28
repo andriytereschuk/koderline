ada3fe0a7654512b8926d63aee6a1e012ea81eae
Andrii <shatskyat@gmail.com>
2014-09-28 20:06:19 +0300

    projects and testimonals

---
+++ inc/projects-list.php
@@ -0,0 +1,170 @@
+
+
+<section class="projects-list pb-30">
+	<div class="container">
+		<div class="row">
+			<div class="span12">
+				<div class="pro">
+					<a href="#" class="project-img">
+						<img src="images/pro1.jpg" alt="">
+						<span class="border-overlay">
+							<span class="bg-overlay"></span>
+							<i class="icon-plus"></i>
+						</span>						
+					</a>
+
+					 <div class="project-info">
+					 	<h2>Внедрение финансового учета на базе 1С Предприятие 8</h2>
+
+					 	 <div class="h">Компания:</div>
+					 	 <p>Стройтрансгаз</p>
+
+					 	 <div class="h">Отрасль:</div>
+					 	 <p>Строительство и лизинг</p>
+
+					 	 <div class="h">Компетенция:</div>
+					 	 <p>Документооборот</p>
+
+					 	 <div class="h">Продукт:</div>
+					 	 <p>1C: <span class="red">Документооборот 8</span></p>
+
+					 	 <div class="h">Статус:</div>
+					 	 <p>Завершенный проект</p>					 	 
+					 </div>
+
+					 <div class="slider-comment">
+					 	<div class="com-h">Отзыв:</div>
+					 	<div class="mark-h">Оценка:</div>
+					 	<div class="mark"><img src="images/temp-marks2.png" alt=""></div>
+					 	<div class="clr"></div>
+					 	<div class="comm">
+					 		<p>Всё очень поравилось! Ни одного штриха не мог бы я сделать,
+					 		 а никогда не был таким большим художником,
+					 		  как в эти минуты. Когда от милой моей долины поднимается пар
+					 		   и полдневное солнце стоит над непроницаемой чащей темного леса и</p>
+
+							 <div class="comm-poster">Главный бухгалтер, компания Стротрансгаз. Игорь Марутяк, 2014</div>					 		   
+
+					 		   <a href="#" class="read-more"><i class="icon-plus-blue"></i><span>Подробнее о проекте</span></a>
+					 	</div>
+					 	
+					 </div>	
+
+					 <div class="clr"></div>				
+				</div>
+
+
+				<div class="pro">
+					<a href="#" class="project-img">
+						<img src="images/pro2.jpg" alt="">
+						<span class="border-overlay">
+							<span class="bg-overlay"></span>
+							<i class="icon-plus"></i>
+						</span>						
+					</a>
+
+					 <div class="project-info">
+					 	<h2>Внедрение финансового учета на базе 1С Предприятие 8</h2>
+
+					 	 <div class="h">Компания:</div>
+					 	 <p>Стройтрансгаз</p>
+
+					 	 <div class="h">Отрасль:</div>
+					 	 <p>Строительство и <span class="red">лизинг</span></p>
+
+					 	 <div class="h">Компетенция:</div>
+					 	 <p>Документооборот</p>
+
+					 	 <div class="h">Продукт:</div>
+					 	 <p>1C: Документооборот 8</p>
+
+					 	 <div class="h">Статус:</div>
+					 	 <p>Завершенный проект</p>					 	 
+					 </div>
+
+					 <div class="slider-comment">
+					 	<div class="com-h">Отзыв:</div>
+					 	<div class="mark-h">Оценка:</div>
+					 	<div class="mark"><img src="images/temp-marks2.png" alt=""></div>
+					 	<div class="clr"></div>
+					 	<div class="comm">
+					 		<p>Всё очень поравилось! Ни одного штриха не мог бы я сделать,
+					 		 а никогда не был таким большим художником,
+					 		  как в эти минуты. Когда от милой моей долины поднимается пар
+					 		   и полдневное солнце стоит над непроницаемой чащей темного леса и</p>
+
+							 <div class="comm-poster">Главный бухгалтер, компания Стротрансгаз. Игорь Марутяк, 2014</div>					 		   
+
+					 		   <a href="#" class="read-more"><i class="icon-plus-blue"></i><span>Подробнее о проекте</span></a>
+					 	</div>
+					 	
+					 </div>	
+
+					 <div class="clr"></div>				
+				</div>
+
+
+				<div class="pro">
+					<a href="#" class="project-img">
+						<img src="images/pro3.jpg" alt="">
+						<span class="border-overlay">
+							<span class="bg-overlay"></span>
+							<i class="icon-plus"></i>
+						</span>						
+					</a>
+
+					 <div class="project-info">
+					 	<h2>Внедрение финансового учета на базе 1С Предприятие 8</h2>
+
+					 	 <div class="h">Компания:</div>
+					 	 <p>Стройтрансгаз</p>
+
+					 	 <div class="h">Отрасль:</div>
+					 	 <p>Строительство и лизинг</p>
+
+					 	 <div class="h">Компетенция:</div>
+					 	 <p>Документооборот</p>
+
+					 	 <div class="h">Продукт:</div>
+					 	 <p>1C: Документооборот 8</p>
+
+					 	 <div class="h">Статус:</div>
+					 	 <p>Завершенный проект</p>					 	 
+					 </div>
+
+					 <div class="slider-comment">
+					 	<div class="com-h">Отзыв:</div>
+					 	<div class="mark-h">Оценка:</div>
+					 	<div class="mark"><img src="images/temp-marks2.png" alt=""></div>
+					 	<div class="clr"></div>
+					 	<div class="comm">
+					 		<p>Всё очень поравилось! Ни одного штриха не мог бы я сделать,
+					 		 а никогда не был таким большим художником,
+					 		  как в эти минуты. Когда от милой моей долины поднимается пар
+					 		   и полдневное солнце стоит над непроницаемой чащей темного леса и</p>
+
+							 <div class="comm-poster">Главный бухгалтер, компания Стротрансгаз. Игорь Марутяк, 2014</div>					 		   
+
+					 		   <a href="#" class="read-more"><i class="icon-plus-blue"></i><span>Подробнее о проекте</span></a>
+					 	</div>
+					 	
+					 </div>	
+
+					 <div class="clr"></div>				
+				</div>								
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="span12">
+				<div class="pagination">
+					<a href="#" class="prev-page">&nbsp;</a>
+					<a href="#">2</a>
+					<a href="#" class="active">3</a>
+					<a href="#">4</a>
+					<a href="#" class="next-page">&nbsp;</a>
+				</div>
+			</div>	
+		</div>
+	</div>	
+</section>
---
+++ projects.php
@@ -0,0 +1,10 @@
+<?php
+	include_once('inc/head.php');
+	include_once('inc/header.php');
+
+	include_once('inc/project-filter.php');
+	include_once('inc/projects-list.php');
+	include_once('inc/offers-small.php');	
+
+	include_once('inc/footer.php');
+?>
