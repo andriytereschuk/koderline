1f931799e01b83a16b3030a28c705442eb427afa
Andrii <shatskyat@gmail.com>
2014-10-21 09:55:09 +0300

    повністю стр Підтримка, 50% - стр Внедрения

---
+++ implement.php
@@ -0,0 +1,13 @@
+<?php
+	include_once('inc/head.php');
+	include_once('inc/header.php');
+
+	include_once('inc/implement/product-top.php');
+	include_once('inc/implement/implem.php');
+	include_once('inc/implement/product-info.php');
+	include_once('inc/implement/product-info2.php');
+	include_once('inc/our-systems.php');
+	include_once('inc/estimate.php');
+	
+	include_once('inc/footer.php');
+?>
--- support.php
+++ support.php
@@ -4,9 +4,12 @@
 
 	include_once('inc/support/product-top.php');
 	include_once('inc/support/choose-product.php');
-	include_once('inc/estimate.php');
-	include_once('inc/main-slider.php');
-	include_once('inc/our-systems.php');	
-
+	include_once('inc/support/product-info.php');
+	include_once('inc/support/product-prop.php');
+	include_once('inc/support/product-info2.php');
+	include_once('inc/support/product-prop2.php');
+	include_once('inc/support/product-info3.php');
+	include_once('inc/support/itc.php');
+	
 	include_once('inc/footer.php');
 ?>
