4e78e3f88d97eb6178f2556641eba087e8c7c58e
Andrii <shatskyat@gmail.com>
2014-10-22 11:44:19 +0300

    pages Implementation and License

--- implement.php
+++ implement.php
@@ -6,6 +6,10 @@
 	include_once('inc/implement/implem.php');
 	include_once('inc/implement/product-info.php');
 	include_once('inc/implement/product-info2.php');
+	include_once('inc/implement/anketa.php');
+	include_once('inc/implement/product-info3.php');
+	include_once('inc/implement/product-info4.php');
+	include_once('inc/implement/product-improve.php');
 	include_once('inc/our-systems.php');
 	include_once('inc/estimate.php');
 	
---
+++ license.php
@@ -0,0 +1,11 @@
+<?php
+	include_once('inc/head.php');
+	include_once('inc/header.php');
+
+	include_once('inc/license/product-top.php');
+	include_once('inc/estimate.php');
+	include_once('inc/license/order.php');
+	include_once('inc/our-systems.php');
+	
+	include_once('inc/footer.php');
+?>
