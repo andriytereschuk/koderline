f940d62b7852f4e83a080c3ce7e5e73f6cdc642a
andrii.tereshchuk <andrii.tereshchuk@eleks.com>
2015-01-21 21:04:52 +0200

    page ERP

---
+++ erp.php
@@ -0,0 +1,13 @@
+<?php
+	include_once('inc/head.php');
+	include_once('inc/header.php');
+
+	include_once('inc/erp/erp-top.php');
+	include_once('inc/erp/erp-manage.php');
+	include_once('inc/erp/erp-order.php');
+	include_once('inc/erp/erp-areas.php');
+	include_once('inc/erp/erp-func.php');
+	include_once('inc/erp/our-services.php');	
+
+	include_once('inc/footer.php');
+?>
