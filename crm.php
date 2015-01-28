abcafbc65e84d1d486c66dae3939edc768c85bd9
andriytereschuk <shatskyat@gmail.com>
2015-01-28 05:53:19 +0200

    CRM

---
+++ crm.php
@@ -0,0 +1,14 @@
+<?php
+	include_once('inc/head.php');
+	include_once('inc/header.php');
+
+	include_once('inc/crm/crm-top.php');
+	include_once('inc/crm/crm-solution.php');
+	include_once('inc/crm/crm-call.php');
+	include_once('inc/crm/crm-areas.php');
+	include_once('inc/crm/crm-manage.php');
+  include_once('inc/crm/crm-able.php');
+	include_once('inc/crm/our-services.php');	
+
+	include_once('inc/footer.php');
+?>
