ede2f8bd1b1f9490c9862138ef8c724a72d81f75
andriytereschuk <shatskyat@gmail.com>
2015-01-30 10:07:46 +0200

    byreniya

---
+++ byreniya.php
@@ -0,0 +1,13 @@
+<?php
+	include_once('inc/head.php');
+	include_once('inc/header.php');
+
+	include_once('inc/byreniya/crm-top.php');
+	include_once('inc/byreniya/crm-solution.php');
+	include_once('inc/byreniya/crm-call.php');
+	include_once('inc/byreniya/crm-areas.php');
+	include_once('inc/estimate.php');	
+	include_once('inc/byreniya/our-systems.php');	
+
+	include_once('inc/footer.php');
+?>
