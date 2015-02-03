518b47fc20f1b1017291fd85e26d9aa55069abc0
andriytereschuk <shatskyat@gmail.com>
2015-02-03 09:59:04 +0200

    part of Re-marketing page

---
+++ re-marketing.php
@@ -0,0 +1,14 @@
+<?php
+	include_once('inc/head.php');
+	include_once('inc/header.php');
+
+	include_once('inc/re-marketing/product-top.php');
+	include_once('inc/re-marketing/product-info.php');
+	include_once('inc/re-marketing/crm-areas.php');
+  include_once('inc/re-marketing/support.php');
+  include_once('inc/main-slider.php'); 
+	include_once('inc/estimate.php');	
+	include_once('inc/re-marketing/our-systems.php');	
+
+	include_once('inc/footer.php');
+?>
