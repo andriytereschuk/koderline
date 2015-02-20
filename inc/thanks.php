42b18094ef64e79518720df2821fb51b28899c1a
andrii.tereshchuk <andrii.tereshchuk@eleks.com>
2015-02-20 14:46:11 +0200

    thanks

---
+++ inc/thanks.php
@@ -0,0 +1,29 @@
+
+
+<section class="filter">
+	<div class="container">
+		<div class="row">
+			<div class="span12">
+				<div class="top-section">
+					<div class="up-section">
+						<i class="icon-check-grey"></i>
+					</div>
+					<h1>Спасибо</h1>
+					<h2>Ваша заявка успешно отправлена</h2>					
+				</div>				
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="span10 offset1">
+				<div class="form-wrap form-small form-padding">
+						<h3>В ближайшие время мы предоставим Вам полный доступ к программе - Koderline:CRM </h3>
+						<a href="#" class="get-permission">
+							<i class="icon-logout"></i>
+							<span>Вернуться на сайт</span>
+						</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
