d24e9d613e25d0696691e395c845fc00bf2e5d29
andriytereschuk <shatskyat@gmail.com>
2015-05-13 10:47:50 +0300

    another landings

---
+++ inc/landing/top4.php
@@ -0,0 +1,24 @@
+
+
+<section class="landing-top white landing landing4">
+	<div class="container">
+	    <div class="row">
+	        <div class="span8">
+	           <h1>Обслуживание и <br> доработка 1С</h1> 
+	        </div>
+	    </div>
+		<div class="row">
+			<div class="span7">
+	           <h2>
+	               Услуги сертифицированных 1С программистов от 1290 руб./час
+	           </h2>
+	           <ul>
+	               <li>Выделений разработчик;</li>
+	               <li>Обслуживание на месте или удаленно;</li>
+	               <li>Предварительная оценка стоимости работ.</li>
+	           </ul>
+	           <a href="#" class="blue-btn"><i class="icon-edit-white"></i>ОСТАВИТЬ ЗАЯВКУ</a>
+			</div>
+		</div>	
+	</div>	
+</section>
---
+++ landing3.php
@@ -0,0 +1,16 @@
+<?php
+	include_once('inc/head.php');
+	include_once('inc/header.php');
+
+	include_once('inc/landing/top3.php');
+	include_once('inc/landing/work-with-us.php');
+	include_once('inc/landing/services-c1.php');
+	include_once('inc/landing/finance.php');
+	include_once('inc/main-slider.php');
+	include_once('inc/landing/warranty.php');
+	include_once('inc/landing/contact-us.php');
+	include_once('inc/landing/feedback.php');
+	include_once('inc/landing/faq.php');
+
+	include_once('inc/footer.php');
+?>
