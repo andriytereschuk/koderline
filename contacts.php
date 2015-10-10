5eced0b17b74e9941ba57ddb86ba2d32ffd48ded
andriytereschuk <shatskyat@gmail.com>
2015-10-10 23:37:36 +0300

    sections Get To and Offices in Contacts

--- contacts.php
+++ contacts.php
@@ -3,8 +3,9 @@
 	include_once('inc/header.php');
 
 	include_once('inc/address.php');
-	include_once('inc/managers.php');
-	include_once('inc/map.php');			
+	include_once('inc/contacts/get.php');
+	include_once('inc/managers.php');		
+	include_once('inc/contacts/offices.php');		
 
 	include_once('inc/footer.php');
 ?>
---
+++ inc/contacts/offices.php
@@ -0,0 +1,161 @@
+
+
+<section class="offices">
+	<div class="container">
+		<hr class="bottom-border">
+		<div class="row">
+			<div class="span12">
+				<div class="small-h">
+					<h2>Наши офисы</h2>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="span6">
+				<div class="country-name">
+					<span>Россия</span>
+				</div>
+
+				<!-- ========= -->
+
+				<div class="office">
+					<div class="office-city">Москва</div>
+					<div class="office-flex">
+						<div class="office-info">
+							<div class="office-icon">
+								<i class="icon-place-small"></i>
+							</div>
+							<div class="office-text">
+								ул. Большая Семеновская д.45,оф.204 
+							</div>
+						</div>
+
+						<div class="office-info">
+							<div class="office-icon">
+								<i class="icon-phone-small"></i>
+							</div>
+							<div class="office-text">
+								+7 (903) 127-91-52
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- ========= -->
+
+				<div class="office">
+					<div class="office-city">Санкт-Петербург</div>
+					<div class="office-flex">
+						<div class="office-info">
+							<div class="office-icon">
+								<i class="icon-place-small"></i>
+							</div>
+							<div class="office-text">
+								ул. Моисеенко, 14 
+							</div>
+						</div>
+
+						<div class="office-info">
+							<div class="office-icon">
+								<i class="icon-phone-small"></i>
+							</div>
+							<div class="office-text">
+								+7 (495) 374 55 29 
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- ========= -->
+
+
+			</div>
+
+			<div class="span6">
+				<div class="country-name">
+					<span>Украина</span>
+				</div>
+
+				<!-- ========= -->
+
+				<div class="office">
+					<div class="office-city">Киев</div>
+					<div class="office-flex">
+						<div class="office-info">
+							<div class="office-icon">
+								<i class="icon-place-small"></i>
+							</div>
+							<div class="office-text">
+								ул. Волгоградская д. 33, к. 124 
+							</div>
+						</div>
+
+						<div class="office-info">
+							<div class="office-icon">
+								<i class="icon-phone-small"></i>
+							</div>
+							<div class="office-text">
+								+3 (044) 222 73 76 
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- ========= -->
+
+				<div class="office">
+					<div class="office-city">Львов</div>
+					<div class="office-flex">
+						<div class="office-info">
+							<div class="office-icon">
+								<i class="icon-place-small"></i>
+							</div>
+							<div class="office-text">
+								ул. Стороженко, 14 
+							</div>
+						</div>
+
+						<div class="office-info">
+							<div class="office-icon">
+								<i class="icon-phone-small"></i>
+							</div>
+							<div class="office-text">
+								+3 (032) 253 07 03
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- ========= -->
+
+				<div class="office">
+					<div class="office-city">Золочев </div>
+
+					<div class="office-flex">
+						<div class="office-info">
+							<div class="office-icon">
+								<i class="icon-place-small"></i>
+							</div>
+							<div class="office-text">
+								Кармелюка 12, оф.10
+							</div>
+						</div>
+
+						<div class="office-info">
+							<div class="office-icon">
+								<i class="icon-phone-small"></i>
+							</div>
+							<div class="office-text">
+								+3 (032) 253 07 03
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<!-- ========= -->
+
+
+			</div>
+		</div>
+</section>
