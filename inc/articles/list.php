aa181eba4b4fa73ba6dfc14ab80e41e51e00c973
andriytereschuk <shatskyat@gmail.com>
2015-10-13 10:57:46 +0300

    articles

---
+++ inc/articles/list.php
@@ -0,0 +1,178 @@
+
+
+<section class="article-list">
+	<div class="container">
+		<div class="row">
+			<div class="span12">
+				<div class="small-h">
+					<h2>Бухгалтерский и налоговый учёт</h2>
+					<div class="article-count"><span>6 статей</span></div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row pb-20">
+			<div class="span4">
+				<div class="article-item">
+					<a href="" class="ar-img">
+						<img src="images/article-item.jpg" alt="">
+						<i class="icon-right-white"></i>
+						<span class="ar-inf">
+							<span class="ar-data">27 Сентября 2015</span>
+							<span class="ar-author">Олег Демиденко</span>
+						</span>
+					</a>
+
+					<div class="ar-desc">
+						<a href="" class="ar-link">Переход с УПП на ERP. Когда и почему стоит переходить на новою 1С:ERP 2.0</a>
+						<p>Выпуск решения 1С:ERP стал новым шагом в развитии продуктов 1С. Можно смело заявлять.. <a href="">Читать далее</a></p>
+
+						<div class="ar-tags">
+							<a href="">1C:ERP</a>
+							<a href="">1С:УПП</a>
+							<a href="">переход на ERP</a>
+							<a href="">переход на ERP</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="span4">
+				<div class="article-item">
+					<a href="" class="ar-img">
+						<img src="images/article-item.jpg" alt="">
+						<i class="icon-right-white"></i>
+						<span class="ar-inf">
+							<span class="ar-data">27 Сентября 2015</span>
+							<span class="ar-author">Олег Демиденко</span>
+						</span>
+					</a>
+
+					<div class="ar-desc">
+						<a href="" class="ar-link">Переход с УПП на ERP. Когда и почему стоит переходить на новою 1С:ERP 2.0</a>
+						<p>Выпуск решения 1С:ERP стал новым шагом в развитии продуктов 1С. Можно смело заявлять.. <a href="">Читать далее</a></p>
+
+						<div class="ar-tags">
+							<a href="">1C:ERP</a>
+							<a href="">1С:УПП</a>
+							<a href="">переход на ERP</a>
+							<a href="">переход на ERP</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="span4">
+				<div class="article-item">
+					<a href="" class="ar-img">
+						<img src="images/article-item.jpg" alt="">
+						<i class="icon-right-white"></i>
+						<span class="ar-inf">
+							<span class="ar-data">27 Сентября 2015</span>
+							<span class="ar-author">Олег Демиденко</span>
+						</span>
+					</a>
+
+					<div class="ar-desc">
+						<a href="" class="ar-link">Переход с УПП на ERP. Когда и почему стоит переходить на новою 1С:ERP 2.0</a>
+						<p>Выпуск решения 1С:ERP стал новым шагом в развитии продуктов 1С. Можно смело заявлять.. <a href="">Читать далее</a></p>
+
+						<div class="ar-tags">
+							<a href="">1C:ERP</a>
+							<a href="">1С:УПП</a>
+							<a href="">переход на ERP</a>
+							<a href="">переход на ERP</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="span4">
+				<div class="article-item">
+					<a href="" class="ar-img">
+						<img src="images/article-item.jpg" alt="">
+						<i class="icon-right-white"></i>
+						<span class="ar-inf">
+							<span class="ar-data">27 Сентября 2015</span>
+							<span class="ar-author">Олег Демиденко</span>
+						</span>
+					</a>
+
+					<div class="ar-desc">
+						<a href="" class="ar-link">Переход с УПП на ERP. Когда и почему стоит переходить на новою 1С:ERP 2.0</a>
+						<p>Выпуск решения 1С:ERP стал новым шагом в развитии продуктов 1С. Можно смело заявлять.. <a href="">Читать далее</a></p>
+
+						<div class="ar-tags">
+							<a href="">1C:ERP</a>
+							<a href="">1С:УПП</a>
+							<a href="">переход на ERP</a>
+							<a href="">переход на ERP</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="span4">
+				<div class="article-item">
+					<a href="" class="ar-img">
+						<img src="images/article-item.jpg" alt="">
+						<i class="icon-right-white"></i>
+						<span class="ar-inf">
+							<span class="ar-data">27 Сентября 2015</span>
+							<span class="ar-author">Олег Демиденко</span>
+						</span>
+					</a>
+
+					<div class="ar-desc">
+						<a href="" class="ar-link">Переход с УПП на ERP. Когда и почему стоит переходить на новою 1С:ERP 2.0</a>
+						<p>Выпуск решения 1С:ERP стал новым шагом в развитии продуктов 1С. Можно смело заявлять.. <a href="">Читать далее</a></p>
+
+						<div class="ar-tags">
+							<a href="">1C:ERP</a>
+							<a href="">1С:УПП</a>
+							<a href="">переход на ERP</a>
+							<a href="">переход на ERP</a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="span4">
+				<div class="article-item">
+					<a href="" class="ar-img">
+						<img src="images/article-item.jpg" alt="">
+						<i class="icon-right-white"></i>
+						<span class="ar-inf">
+							<span class="ar-data">27 Сентября 2015</span>
+							<span class="ar-author">Олег Демиденко</span>
+						</span>
+					</a>
+
+					<div class="ar-desc">
+						<a href="" class="ar-link">Переход с УПП на ERP. Когда и почему стоит переходить на новою 1С:ERP 2.0</a>
+						<p>Выпуск решения 1С:ERP стал новым шагом в развитии продуктов 1С. Можно смело заявлять.. <a href="">Читать далее</a></p>
+
+						<div class="ar-tags">
+							<a href="">1C:ERP</a>
+							<a href="">1С:УПП</a>
+							<a href="">переход на ERP</a>
+							<a href="">переход на ERP</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="span12">
+				<div class="pagination">
+					<a href="#" class="prev-page">&nbsp;</a>
+					<a href="#">2</a>
+					<a href="#" class="active">3</a>
+					<a href="#">4</a>
+					<a href="#" class="next-page">&nbsp;</a>
+				</div>
+			</div>	
+		</div>
+	</div>	
+</section>
