ccbe03f32a6a9640f355f745a4aeb2b7c256f432
andriytereschuk <shatskyat@gmail.com>
2016-02-09 20:28:41 +0200

    menu fixed

--- js/functions.js
+++ js/functions.js
@@ -5,6 +5,25 @@ $(function() {
 		$("html, body").animate({ scrollTop: 0 }, '600', 'swing');
 		return false;
 	});
+
+	// fixed menu
+	function checkScroll() {
+		if ($(window).scrollTop()  > 72) {
+		  $('.menu').addClass('menu-fixed');  
+		  $('.top').addClass('mb-52');  
+		}
+		else
+		{
+		  $('.menu').removeClass('menu-fixed');   
+		  $('.top').removeClass('mb-52');   
+		} 
+	}
+
+	$(window).scroll(function () {
+		checkScroll();
+	});
+
+	checkScroll();
 });
 
 // Forms
