d28b12f1d8270151b91a387ced50a6de304e4220
andriytereschuk <shatskyat@gmail.com>
2016-02-18 10:10:48 +0200

    shcdule one - in progress

---
+++ inc/schedule/schedule-one.php
@@ -0,0 +1,63 @@
+
+
+<section class="schedule-one bg-grey">
+	<div class="container">
+		<h2>Вебинар "Управление договорной работой с помощью программы 1С:Документооборот"</h2>
+		<hr>
+
+		<div class="row">
+			<div class="span9">
+				<div class="schedule-event">
+					<div class="schedule-event-col-1">
+
+						<div class="schedule-event-item">
+							<div class="schedule-event-item__icon">
+								<i class="icon-cal-blue"></i>
+							</div>							
+							<div class="schedule-event-item__text">22 января</div>
+						</div>
+
+						<div class="schedule-event-item">
+							<div class="schedule-event-item__icon">
+								<i class="icon-clock-blue"></i>
+							</div>							
+							<div class="schedule-event-item__text">15.00-17.00</div>
+						</div>
+
+					</div>
+					<div class="schedule-event-col-2">
+						
+						<div class="schedule-event-item">
+							<div class="schedule-event-item__icon">
+								<i class="icon-pin-blue"></i>
+							</div>							
+							<div class="schedule-event-item__text">Москва</div>
+						</div>
+
+						<div class="schedule-event-item">
+							<div class="schedule-event-item__icon">
+								<i class="icon-phone-blue"></i>
+							</div>						
+							<div class="schedule-event-item__text">+7 (495) 374-55-29</div>
+						</div>
+
+					</div>
+					<div class="schedule-event-col-3">
+
+						<div class="schedule-event-item">
+							<div class="schedule-event-item__text align-center">Цена: бесплатно</div>
+						</div>
+
+						<div class="schedule-event-item">
+							<a href="" class="blue-btn">Записаться</a>
+						</div>
+
+					</div>
+
+					<div class="clr"></div>
+				</div>
+			</div>
+			<div class="span3"></div>
+		</div>
+	</div>	
+</section>
