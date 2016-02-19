c973f801923e604d7f3987668931d657781a11b7
andriytereschuk <shatskyat@gmail.com>
2016-02-19 09:13:14 +0200

    schedule one - fixing

--- inc/schedule/schedule-one.php
+++ inc/schedule/schedule-one.php
@@ -56,8 +56,107 @@
 
 					<div class="clr"></div>
 				</div>
+
+				<div class="schedule-info">
+					<div class="row">
+						<div class="span1">
+							<div class="schedule-info-icon">
+								<i class="icon-earphones"></i>
+							</div>
+						</div>
+
+						<div class="span8">
+							<h3>Целевая аудитория</h3>
+							<p>Предприниматели, бухгалтера.</p>
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="span1">
+							<div class="schedule-info-icon">
+								<i class="icon-note-blue"></i>
+							</div>
+						</div>
+
+						<div class="span8">
+							<h3>Описание</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. </p>
+						</div>
+					</div>
+
+
+					<div class="row">
+						<div class="span1">
+							<div class="schedule-info-icon">
+								<i class="icon-list-blue"></i>
+							</div>
+						</div>
+
+						<div class="span8">
+							<h3>Программа</h3>
+
+							<ul>
+								<li>- Duis vulputate commodo</li>
+								<li>- Sed rhoncus, tortor sed</li>
+								<li>- Quisque nec mauris sit amet</li>
+								<li>- Lorem ipsum dolor sit amet</li>
+								<li>- Nulla quam velit</li>
+							</ul>
+
+						</div>
+					</div>
+
+					<div class="row">
+						<div class="span1">
+							<div class="schedule-info-icon">
+								<i class="icon-user-blue"></i>
+							</div>
+						</div>
+
+						<div class="span8">
+							<h3>Ведущий: Тенгиз Куправа</h3>
+
+							<img src="images/speacker1.jpg" alt="">
+
+							<ul>
+								<li>- Лучший специалист-разработчик.</li>
+								<li>- Автор доработки "Бухгалтерия 8.3" </li>
+								<li>- Постоянный консультант компании "Кодерлайн" по вопросам внедрения.</li>
+							</ul>
+
+						</div>
+					</div>
+				</div>
+
+				<hr>
+
+				<div class="schedule-invite">
+					<p>Вход на мероприятие свободный. Предварительная....</p>
+					<a href="" class="blue-btn">Записаться</a>
+				</div>
 			</div>
-			<div class="span3"></div>
+			<div class="span3">
+				<div class="schedule-other">
+					<div class="schedule-other-h">Другие вебинары:</div>
+
+					<ul>
+						<li>
+							<span>21 июля 2014</span>
+							<a href="">“Диагностика типовых узких мест  ...</a>
+						</li>
+
+						<li>
+							<span>21 июля 2014</span>
+							<a href="">“Диагностика типовых узких мест  ...</a>
+						</li>
+
+						<li>
+							<span>21 июля 2014</span>
+							<a href="">“Диагностика типовых узких мест  ...</a>
+						</li>
+					</ul>
+				</div>
+			</div>
 		</div>
 	</div>	
 </section>
