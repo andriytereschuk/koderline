07e62dfb5a890b4973cb94f62e8ba45200dd7d84
andriytereschuk <shatskyat@gmail.com>
2016-02-25 10:32:01 +0200

    schedule / without calendar

---
+++ inc/schedule/schedule-calendar.php
@@ -0,0 +1,155 @@
+
+<section class="filter schedule">
+	<div class="container">
+		<div class="row">
+			<div class="span12">
+				<div class="top-section">
+					<div class="up-section">
+						<i class="icon-grey-kalendar"></i>
+					</div>
+					<h1>Расписание мероприятий</h1>
+					<h2>Вебинары, мастер-классы, семинары</h2>					
+				</div>				
+			</div>
+		</div>
+
+		<div class="row schedule-cal">
+			<div class="span5"></div>
+			<div class="span7">
+				<div class="schedule-cal-title">Особенности учета НДС в программе «1С: Бухгалтерия предприятия 8 (редакция 3.0)»</div>
+				<div class="schedule-cal-info">
+					<div class="schedule-cal-prop">22 января</div>
+					<div class="schedule-cal-prop">15.00-17.00</div>
+					<div class="schedule-cal-prop">Москва</div>
+					<div class="schedule-cal-prop">Цена: бесплатно</div>
+				</div>
+
+				<div class="f-tags">
+					<a href="">1С бухгалтерия</a>
+					<a href="">1С 8.3</a>
+					<a href="">1С 8.3</a>
+					<a href="">1С Управление торговлей</a>
+					<a href="">1С 8.3</a>
+					<a href="">1С Управление торговлей</a>
+					<a href="">1С бухгалтерия</a>
+					<a href="">1С Управление торговлей</a>
+					<a href="">1С бухгалтерия</a>
+				</div>
+
+				<a href="" class="blue-btn">ЗАРЕГИСТРИРОВАТЬСЯ</a>
+			</div>
+		</div>
+	</div>
+
+	<div class="container container-white">
+		<div class="row">
+			<div class="span4">
+				<select class="filter-select">
+					<option>Тип мероприятия</option>
+					<option>Вид №2</option>
+					<option>Вид №3</option>
+				</select>
+			</div>
+			<div class="span4">
+				<select class="filter-select">
+					<option>Направление</option>
+					<option>Вид №2</option>
+					<option>Вид №3</option>
+				</select>				
+			</div>
+			<div class="span4">
+				<select class="filter-select">
+					<option>Бизнес задача</option>
+					<option>Вид №2</option>
+					<option>Вид №3</option>
+				</select>				
+			</div>				
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="schedule-item">
+			<div class="schedule-item-left">
+				<div class="schedule-cal-title">Особенности учета НДС в программе «1С: Бухгалтерия предприятия 8 (редакция 3.0)»</div>
+				<div class="schedule-cal-info">
+					<div class="schedule-cal-prop">22 января</div>
+					<div class="schedule-cal-prop">15.00-17.00</div>
+					<div class="schedule-cal-prop">Москва</div>
+					<div class="schedule-cal-prop">Цена: бесплатно</div>
+				</div>
+
+				<div class="f-tags">
+					<a href="">1С бухгалтерия</a>
+					<a href="">1С 8.3</a>
+					<a href="">1С 8.3</a>
+					<a href="">1С Управление торговлей</a>
+				</div>
+			</div>
+
+			<div class="schedule-item-right">
+				<a href="" class="blue-btn">Записаться</a>
+				<a href="">Подробнее</a>
+			</div>
+		</div>
+
+		<div class="schedule-item">
+			<div class="schedule-item-left">
+				<div class="schedule-cal-title">Особенности учета НДС в программе «1С: Бухгалтерия предприятия 8 (редакция 3.0)»</div>
+				<div class="schedule-cal-info">
+					<div class="schedule-cal-prop">22 января</div>
+					<div class="schedule-cal-prop">15.00-17.00</div>
+					<div class="schedule-cal-prop">Москва</div>
+					<div class="schedule-cal-prop">Цена: бесплатно</div>
+				</div>
+
+				<div class="f-tags">
+					<a href="">1С бухгалтерия</a>
+					<a href="">1С 8.3</a>
+					<a href="">1С 8.3</a>
+					<a href="">1С Управление торговлей</a>
+				</div>
+			</div>
+
+			<div class="schedule-item-right">
+				<a href="" class="blue-btn">Записаться</a>
+				<a href="">Подробнее</a>
+			</div>
+		</div>
+
+		<div class="schedule-item">
+			<div class="schedule-item-left">
+				<div class="schedule-cal-title">Особенности учета НДС в программе «1С: Бухгалтерия предприятия 8 (редакция 3.0)»</div>
+				<div class="schedule-cal-info">
+					<div class="schedule-cal-prop">22 января</div>
+					<div class="schedule-cal-prop">15.00-17.00</div>
+					<div class="schedule-cal-prop">Москва</div>
+					<div class="schedule-cal-prop">Цена: бесплатно</div>
+				</div>
+
+				<div class="f-tags">
+					<a href="">1С бухгалтерия</a>
+					<a href="">1С 8.3</a>
+					<a href="">1С 8.3</a>
+					<a href="">1С Управление торговлей</a>
+				</div>
+			</div>
+
+			<div class="schedule-item-right">
+				<a href="" class="blue-btn">Записаться</a>
+				<a href="">Подробнее</a>
+			</div>
+		</div>
+
+		<div class="row mt-20">
+			<div class="span12">
+				<div class="pagination">
+					<a href="#" class="prev-page">&nbsp;</a>
+					<a href="#">2</a>
+					<a href="#" class="active">3</a>
+					<a href="#">4</a>
+					<a href="#" class="next-page">&nbsp;</a>
+				</div>
+			</div>	
+		</div>
+	</div>	
+</section>
