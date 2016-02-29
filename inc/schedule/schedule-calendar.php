4889b26bf85f21f075b7e7ffaefd75720872a281
andriytereschuk <shatskyat@gmail.com>
2016-02-29 23:10:16 +0200

    shedule - done

--- inc/schedule/schedule-calendar.php
+++ inc/schedule/schedule-calendar.php
@@ -13,32 +13,51 @@
 			</div>
 		</div>
 
-		<div class="row schedule-cal">
-			<div class="span5"></div>
-			<div class="span7">
-				<div class="schedule-cal-title">Особенности учета НДС в программе «1С: Бухгалтерия предприятия 8 (редакция 3.0)»</div>
-				<div class="schedule-cal-info">
-					<div class="schedule-cal-prop">22 января</div>
-					<div class="schedule-cal-prop">15.00-17.00</div>
-					<div class="schedule-cal-prop">Москва</div>
-					<div class="schedule-cal-prop">Цена: бесплатно</div>
-				</div>
+		<div class="schedule-cal row" id="eventCalendar"></div>
 
-				<div class="f-tags">
-					<a href="">1С бухгалтерия</a>
-					<a href="">1С 8.3</a>
-					<a href="">1С 8.3</a>
-					<a href="">1С Управление торговлей</a>
-					<a href="">1С 8.3</a>
-					<a href="">1С Управление торговлей</a>
-					<a href="">1С бухгалтерия</a>
-					<a href="">1С Управление торговлей</a>
-					<a href="">1С бухгалтерия</a>
-				</div>
+		<!-- http://www.vissit.com/projects/eventCalendar/ -->
+		<script src="js/moment.js"></script>				
+		<script src="js/jquery.eventCalendar.js"></script>
 
-				<a href="" class="blue-btn">ЗАРЕГИСТРИРОВАТЬСЯ</a>
-			</div>
-		</div>
+		<script>
+			$(document).ready(function() {
+				$("#eventCalendar").eventCalendar({
+					eventsjson: 'json/events.json.php',
+					locales: {
+						locale: "ru",
+						monthNames: [ "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+					                "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" ],
+						dayNames: [ 'воскресенье','понедельник','вторник','среда',
+							'четверг','пятница','суббота' ],
+						dayNamesShort: [ "Вс","Пн","Вт","Ср","Чт","Пт","Сб" ],
+						txt_noEvents: "Нет событий в этот период",
+						txt_SpecificEvents_prev: "",
+						txt_SpecificEvents_after: "- события:",
+						txt_next: "",
+						txt_prev: "",
+						txt_NextEvents: "Ближайшие события:",
+						txt_GoToEventUrl: "Смотреть",
+						txt_loading: "Загрузка...",
+						"moment": {
+					        "months" : [ "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+					                "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь" ],
+					        "monthsShort" : [ "Янв", "Фев", "Мар", "Апр", "Май", "Июн",
+					                "Июл", "Авг", "Сен", "Окт", "Ноя", "Дек" ],
+					        "weekdays" : [ "Domingo","Lunes","Martes","Miércoles",
+					                "Jueves","Viernes","Sabado" ],
+					        "weekdaysShort" : [ "Вс","Пн","Вт","Ср",
+					                "Чт","Пт","Сб" ],
+					        "weekdaysMin" : [ "Вс","Пн","Вт","Ср",
+					                "Чт","Пт","Сб" ]
+					    }
+					},
+					jsonDateFormat: 'human',
+					openEventInNewWindow: true,
+					moveSpeed: 10,
+					moveOpacity: 0
+				});
+			});
+		</script>
 	</div>
 
 	<div class="container container-white">
@@ -70,7 +89,7 @@
 	<div class="container">
 		<div class="schedule-item">
 			<div class="schedule-item-left">
-				<div class="schedule-cal-title">Особенности учета НДС в программе «1С: Бухгалтерия предприятия 8 (редакция 3.0)»</div>
+				<div class="schedule-cal-title"><a href="">Особенности учета НДС в программе «1С: Бухгалтерия предприятия 8 (редакция 3.0)»</a></div>
 				<div class="schedule-cal-info">
 					<div class="schedule-cal-prop">22 января</div>
 					<div class="schedule-cal-prop">15.00-17.00</div>
@@ -94,7 +113,7 @@
 
 		<div class="schedule-item">
 			<div class="schedule-item-left">
-				<div class="schedule-cal-title">Особенности учета НДС в программе «1С: Бухгалтерия предприятия 8 (редакция 3.0)»</div>
+				<div class="schedule-cal-title"><a href="">Особенности учета НДС в программе «1С: Бухгалтерия предприятия 8 (редакция 3.0)»</a></div>
 				<div class="schedule-cal-info">
 					<div class="schedule-cal-prop">22 января</div>
 					<div class="schedule-cal-prop">15.00-17.00</div>
@@ -118,7 +137,7 @@
 
 		<div class="schedule-item">
 			<div class="schedule-item-left">
-				<div class="schedule-cal-title">Особенности учета НДС в программе «1С: Бухгалтерия предприятия 8 (редакция 3.0)»</div>
+				<div class="schedule-cal-title"><a href="">Особенности учета НДС в программе «1С: Бухгалтерия предприятия 8 (редакция 3.0)»</a></div>
 				<div class="schedule-cal-info">
 					<div class="schedule-cal-prop">22 января</div>
 					<div class="schedule-cal-prop">15.00-17.00</div>
